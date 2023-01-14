69f2aa769e7216eafc074c89a034f5bb620143c3
Jacob Chatfield <jacob.chatfield45@outlook.com>
2023-01-14 17:14:44 +0000

    First commit which includes my questions in the questions.js file, and beginning some logic in the logic.js file. Currently working on extracting data from the questions so I can use this in future.

---
+++ assets/js/logic.js
@@ -0,0 +1,29 @@
+// timed quiz game
+// starts on the start button being clicked
+// timer which subtracts some time (10 sec) when a question is answered incorrectly
+// next question is shown as soon as the one before is answered
+// final score is questions answered + time remaining
+
+
+// question = 0
+
+// var currentQuestion = 0;
+
+// after checking the answer, + this by one and then the next question should render
+
+
+let correctAnswers;
+
+for(i = 0; i < 5; i++){
+    console.log(questions[i].correctAnsw);
+    
+}
+console.log(correctAnswers);
+// function renderQuestions(){
+//     for (var a = 0; a < questions.length; a++){
+//          console.log(questions[a].answers[0]);
+//     }
+// }
+// question rendered depends on value
+// value increases after question
+
---
+++ assets/js/questions.js
@@ -0,0 +1,95 @@
+// var todoInput = document.querySelector("#todo-text");
+// var todoForm = document.querySelector("#todo-form");
+// var todoList = document.querySelector("#todo-list");
+// var todoCountSpan = document.querySelector("#todo-count");
+
+var questionTitle = document.querySelector("#question-title");
+var questionChoices = document.querySelector('#choices');
+
+
+
+var questions = [
+  {
+    question: "What is a string in javascript?",
+    answers: [
+        "Something that lets you put words",
+        "Something that lets numbers",
+        "A type of function",
+        "A property of the DOM"
+    ],
+    correctAnsw: "Something that lets you put words"
+  },
+  {
+    question: "What is JavaScript used for?",
+    answers: [
+        "Styling a page",
+        "Programming only games",
+        "Programming websites as well as games and some other stuff",
+        "A media website"
+    ],
+    correctAnsw: "Programming websites as well as games and some other stuff"
+  },
+  {
+    question: "What is HTML?",
+    answers: [
+        "Styles pages",
+        "Gives logic to pages",
+        "Provides structure to pages",
+        "A type of media file"
+    ],
+    correctAnsw: "Provides structure to pages"
+  },
+  {
+    question: "What is an array?",
+    answers: [
+        "A way of storing multiple bits of data",
+        "A way of defining a piece of data and giving it a name",
+        "A type of function",
+        "A special character used as an operator"
+    ],
+    correctAnsw: "A way of storing multiple bits of data"
+  },
+  {
+    question: "What tag do you use to link a JS file in your HTML code?",
+    answers: [
+        "<javasc>",
+        "<script>",
+        "<ps>",
+        "<programming>"
+    ],
+    correctAnsw: "<script>"
+  },
+
+
+]
+
+let currentQuestion = 0;
+
+function renderQuestions(){
+    questionTitle.innerHTML = "";
+    questionChoices.innerHTML = "";
+
+    for (let r = 0; r < questions.length; r++){
+        console.log(questions[r]);
+}
+}
+// function renderTodos() {
+//     // Clear todoList element and update todoCountSpan
+//     todoList.innerHTML = "";
+//     todoCountSpan.textContent = todos.length;
+  
+//     // Render a new li for each todo
+//     for (var i = 0; i < todos.length; i++) {
+//       var todo = todos[i];
+  
+//       var li = document.createElement("li");
+//       li.textContent = todo;
+//       li.setAttribute("data-index", i);
+  
+//       var button = document.createElement("button");
+//       button.textContent = "Complete";
+  
+//       li.appendChild(button);    //<li data-index="1">LearnCSS <button>Complete</button></li>
+//       todoList.appendChild(li);
+//     }
+  
